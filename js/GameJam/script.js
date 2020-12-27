4c874eb3f4906a0ab1c1e5ef5cc663ac7b349e41
Jacob Berman <jacobberman1234@gmail.com>
2020-12-26 23:47:56 -0500

    nine commit

--- js/GameJam/script.js
+++ js/GameJam/script.js
@@ -36,3 +36,34 @@ function CloseModal(modal){
     modal.classList.remove('active')
     overlay.classList.remove('active')
 }
+
+function CloseOnSubmit(){
+    const modals = document.querySelectorAll('.modal.active')
+    modals.forEach(modal => {
+        CloseModal(modal)
+    })
+}
+
+//GoogleSheets
+const googleScript = 'https://script.google.com/macros/s/AKfycbwsFTvO7QCRgvDNJmvH7nnOhYf_eqm5YHg96N79y6VPoPeltKms/exec'
+const form = document.forms['google-sheets']
+
+const submitModal = document.querySelector("submitModal")
+
+// const fName = getElementById('fName').value
+// const lName = getElementById('lName').value
+// const email = getElementById('email').value
+// var data = { 'fName' : fName, 'lName' : lName, 'email' : email}
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(googleScript, {
+        method: 'POST', 
+        body: new FormData(form), 
+        headers: {
+        'Content-Type': 'text/plain;charset=utf-8',
+        }
+    })
+        .then(response => alert('Thanks for your interest! Good luck on the Game jam!'))
+        .catch(error => console.error('Error!', error.message))
+    })
